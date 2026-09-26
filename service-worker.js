ea8055f28dbe1ff16455276b897e1eb6326c1631
Sandip Dashboard Publisher <sandeeppatel9099@gmail.com>
2026-09-26 19:27:36 +0530

    Dashboard update

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Sirf app ka dhaancha cache hota he - Supabase ka DATA kabhi cache nahi (privacy + taaza data).
-const CACHE='sandip-dash-946ff455ee';
+const CACHE='sandip-dash-0df13bde60';
 const SHELL=['./','index.html','manifest-ecom.json','ecom-192.png','ecom-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
